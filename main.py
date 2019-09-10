dcc0d10937ccfb0091c2d07f9da2980fd45c0e09
Yi Wei Chen <chen1119@bu.edu>
2019-09-10 19:55:44 -0400

    mainpage code updated

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-
-# @ signifies a decorator - way to wrap a function and modifying its behavior
-# connect url to the return value in the function
 @app.route('/')
-def index():
-    return 'This is main page'  # what user sees
+def root():
+    return render_template(
+        'mainpage.html')
 
-# pass variable in
-# if need to pass integer variable, add <int:whatever_the_name>
-@app.route('/user_login/<username>')
-def user_login(username):
-    return "<h2>Your username is %s</h2>" % username
-
+@app.route('/temp')
+def temp():
+    return render_template(
+        'temptest.html')
 
 if __name__ == '__main__':
+
     app.run()
