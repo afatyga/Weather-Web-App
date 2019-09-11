0ff072f30eba6525196e5fcc1de12c06c3933e3b
Alex <afatyga@bu.edu>
2019-09-10 20:19:14 -0400

    connecting back to front correctly

--- main.py
+++ main.py
@@ -3,17 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# @ signifies a decorator - way to wrap a function and modifying its behavior
-# connect url to the return value in the function
 @app.route('/')
-def index():
-    return 'This is main page'  # what user sees
-
-# pass variable in
-# if need to pass integer variable, add <int:whatever_the_name>
-@app.route('/user_login/<username>')
-def user_login(username):
-    return "<h2>Your username is %s</h2>" % username
+def root():
+    return render_template(
+        'mainpage.html')
 
 
 if __name__ == '__main__':
