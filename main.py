d0d1dc98ee61655fdfc4106b73b9b2da9e9b9105
Yi Wei Chen <chen1119@bu.edu>
2019-09-12 21:26:21 -0400

    main.py issue fixed

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ from flask import Flask, render_template, Response, request
 
 from flask_sqlalchemy import SQLAlchemy
 import logging
+
 logging.basicConfig(level=logging.INFO)
 
 import sqlalchemy
 
 from random import *
+
 app = Flask(__name__)
 import os
 
 db_user = "root"
 db_pass = "47"
-db_name = "sensorData" #database it's reading from!
+db_name = "sensorData"  # database it's reading from!
 cloud_sql_connection_name = "ec463-group47:northamerica-northeast1:instance"
-
 
 db = sqlalchemy.create_engine(
     # Equivalent URL:
-    #mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
+    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
     sqlalchemy.engine.url.URL(
         drivername='mysql+pymysql',
         username=db_user,
@@ -41,18 +42,20 @@ db = sqlalchemy.create_engine(
     pool_recycle=1800,  # 30 minutes
 
 )
-#write random values into 2 lists
-#x = []
-#y = []
-#z = []
-#for i in range(10):
+
+
+# write random values into 2 lists
+# x = []
+# y = []
+# z = []
+# for i in range(10):
 #	a = randint(1, 100)
 #	b = randint(1,100)
 #	x.append(a/100)
 #	y.append(b)
 #	#z.append((a,b))
 
-#with db.connect() as conn: #how to create a new table within the database
+# with db.connect() as conn: #how to create a new table within the database
 #    conn.execute(
 #            "CREATE TABLE entries2 (temp INTEGER, humidity DECIMAL(3,2));"
 #        )
@@ -68,42 +71,43 @@ db = sqlalchemy.create_engine(
 #    conn.execute("INSERT INTO entries2(temp,humidity) VALUES(" + x[9] + "," + y[9] + ");")
 
 
-
 @app.route('/')
 def root():
-	temp = []
-	humidity = []
-	data = []
-	with db.connect() as conn:
-		all_data = conn.execute(
+    temp = []
+    humidity = []
+    data = []
+    with db.connect() as conn:
+        all_data = conn.execute(
             "SELECT * FROM entries;"
-		).fetchall()
-		for row in all_data:
-			temp.append(row[0])
-			humidity.append(row[1])
-         #   data.append({'x': row[0],'y': row[1]})
-	return render_template('mainpage.html', temp = temp, humidity = humidity, length = len(temp))
+        ).fetchall()
+        for row in all_data:
+            temp.append(row[0])
+            humidity.append(row[1])
+    #   data.append({'x': row[0],'y': row[1]})
+    return render_template('mainpage.html', temp=temp, humidity=humidity, length=len(temp))
+
 
 @app.route('/temp')
 def temp():
-	temper = []
-	humid = []
-	data = []
-	with db.connect() as conn:
-		all_data = conn.execute("SELECT * FROM entries;").fetchall()
-		for row in all_data:
-			temper.append(row[0])
-			humid.append(row[1])
-            data.append({'x': row[0],'y': row[1]})
-	length = len(temper)
-	return render_template(
-        'temptest.html', temper =temper, humidity = humid, length = length, data = data)
+    temper = []
+    humid = []
+    data = []
+    with db.connect() as conn:
+        all_data = conn.execute("SELECT * FROM entries;").fetchall()
+        for row in all_data:
+            temper.append(row[0])
+            humid.append(row[1])
+            data.append({'x': row[0], 'y': row[1]})
+    length = len(temper)
+    return render_template(
+        'temptest.html', temper=temper, humidity=humid, length=length, data=data)
+
 
 @app.route('/location')
 def location():
     return render_template(
         'location.html')
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     app.run()
